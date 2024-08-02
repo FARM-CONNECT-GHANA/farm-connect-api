5ea1e67fac5ca8dbdfcb7e495b663c2e549f0586
Busola Tom <busolatom@gmail.com>
2024-08-02 11:01:03 +0000

    api documentation with mickeymond expressOasgenerator

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 import express from "express";
 import cors from 'cors';
+import expressOasGenerator from '@mickeymond/express-oas-generator';
 import { dbConnection } from "./config/db.js";
 import userRoutes from "./routes/user.routes.js";
+import mongoose from "mongoose";
 
 // create express app
 const farmconnectapp = express();
+expressOasGenerator.handleResponses(farmconnectapp, {
+    alwaysServeDocs: true,
+    tags: ['farmerprofile', 'customerprofile'],
+    mongooseModels: mongoose.modelNames(),
+})
 
 // Apply middleware
 farmconnectapp.use(cors({credentials: true, origin: '*'}));
-farmconnectapp.use(express.json());
+farmconnectapp.use(express.json({ limit: '50mb' }));
+farmconnectapp.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+// Use Routes
 farmconnectapp.use(userRoutes)
+
+expressOasGenerator.handleRequests();
+farmconnectapp.use((req, res) => res.redirect('/api-docs/'));
 
 
 // Listen for incoming requests
